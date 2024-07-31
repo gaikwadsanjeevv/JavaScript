e41e2c0850293c245f992ecb60bb236c2029e5d5
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-07-31 07:49:40 -0600

    Create primitives.js
    Primitive Types example

---
+++ primitives.js
@@ -0,0 +1,21 @@
+//string
+let city = "San Jose";
+
+let country;
+country = "USA";
+
+const name = "Monkey"; //once declared cant change
+console.log(name);
+
+//numbers
+
+const age = 20;
+let price = 1.23;
+
+//boolean
+const happy = true;
+let sad = false;
+
+//null and undefined
+const empty = null;
+const empty2 = undefined;
