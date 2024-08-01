bc7397f4579d68b302acb85612601e58b7a6dd32
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-08-01 00:25:17 -0600

    Create MathOperators.js
    mathematical operator

---
+++ MathOperators.js
@@ -0,0 +1,18 @@
+//Mathematical Operators
+let age = 10 + 23;
+console.log(age);
+
+let points = 30-21;
+console.log("The value is: ",points);
+
+let score = 7 * 50;
+console.log(score);
+
+let divide =  100/74;
+console.log(divide);
+
+let mod =  6%5;
+console.log(mod);
+
+let exp = 4 ** 2;
+console.log(exp);
