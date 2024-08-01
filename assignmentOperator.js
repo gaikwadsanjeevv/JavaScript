564a8fac878460eaaf1ff32b810065ceb382a8f8
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-08-01 00:21:55 -0600

    Create assignmentOperator.js
    assignment operator example

---
+++ assignmentOperator.js
@@ -0,0 +1,23 @@
+let point = 20;
+point++;
+console.log(point);
+
+let score = 30;
+score--;
+score--;
+console.log(score);
+
+score += 6;
+console.log(score);
+
+score += 100;
+console.log(score);
+
+let age = 7;
+console.log(age *= 2); 
+//if we use const here we cant operate on it with any operations.
+
+console.log(age %= 6);
+
+console.log(age **= 2);
+
