09214cf3b0790d6a1820afa22e08ddf932801394
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-08-01 00:01:06 -0600

    Create typeof.js
    demo of typeof

---
+++ typeof.js
@@ -0,0 +1,9 @@
+const colour = "blue";
+let eggs = 12;
+const happy = true;
+const emptyThing = null;
+const another_thing = undefined;
+console.log(typeof eggs); 
+console.log(typeof emptyThing);
+console.log(typeof another_thing);
+console.log(typeof happy);
