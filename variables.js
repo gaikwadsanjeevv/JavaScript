7c3f641bbfb9d049ba1bd3b8d075c187d5989d0a
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-26 15:36:03 -0700

    Create variables.js
    variables.demo

---
+++ variables.js
@@ -0,0 +1,18 @@
+var name="Sanjeev";
+var age =20;
+console.log(name);
+console.log(age);
+
+var a = true;
+var b = false;
+var c = 10.8934;
+var d = function() {};
+var e = {};
+var f = [];
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
