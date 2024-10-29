3116525140996aa1094b86658675b671a7aed41b
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-29 13:25:26 -0700

    Create switchCase.js
    choose between multiple colors

---
+++ switchCase.js
@@ -0,0 +1,10 @@
+var color = "magenta";
+
+switch(color) {
+    case "red" : console.log("color is red"); break;
+    case "blue" : console.log("color is blue"); break;
+    case "yellow" : console.log("color is yellow"); break;
+    case "green" : console.log("color is green"); break;
+    case "white" : console.log("color is white"); break;
+    default : console.log("The color wont match"); break;
+}
