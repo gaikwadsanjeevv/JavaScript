028dd48c3b9a2cf2061383e9eaaa52dfe6734bf3
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-28 23:28:23 -0700

    Create logicaL.js
    logical operator demo

---
+++ logicaL.js
@@ -0,0 +1,7 @@
+var a = 10, b = 100, c = 50;
+
+console.log((a == b) && (a > c));
+console.log((a == b) || (a > c));
+console.log((a == b) != (a > c));
+console.log((a != b) && (a < c));
+console.log(!(a==b));
