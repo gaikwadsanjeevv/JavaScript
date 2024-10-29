3a1d522ad9d3dc1fa6201fdec318a8bbde257b45
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-28 23:51:26 -0700

    Backtick.js
    String interpolation with using backtick - and also multi line display as per required positioning using backtick

---
+++ Backtick.js
@@ -0,0 +1,11 @@
+var username = "Sanjeev", age = 20;
+
+var message = `Hey, ${username}, your age is ${age}`;
+console.log(message);
+
+
+var msg = `hey ${username}
+your 
+    age
+             is ${age}`;
+             console.log(msg);
