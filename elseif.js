6831608fafbafa3e189db40e19d47d6e6190207f
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-10-29 12:59:43 -0700

    Create elseif.js
    multiple else if

---
+++ elseif.js
@@ -0,0 +1,13 @@
+var score = 82;
+
+if (score >= 90) {
+    console.log("Grade: A");
+} else if (score >= 80) {
+    console.log("Grade: B");
+} else if (score >= 70) {
+    console.log("Grade: C");
+} else if (score >= 60) {
+    console.log("Grade: D");
+} else {
+    console.log("Grade: F");
+}
