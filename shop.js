881dec86294b8e4bf44e5b1771e2ad3ac442d347
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-04 15:59:48 -0800

    Create shop.js
    demo of object

---
+++ shop.js
@@ -0,0 +1,26 @@
+const product = {
+    title : "Ball pen",
+    rating : 4,
+    offer : 5,
+    price: 270,
+};
+
+console.log(product);
+
+const profile = {
+    username : "sanjeev gaikwad",
+    isfollow: false,
+    followers: 123,
+    following: 123,
+
+};
+console.log(profile);
+
+// output
+{ title: 'Ball pen', rating: 4, offer: 5, price: 270 }
+{
+  username: 'sanjeev gaikwad',
+  isfollow: false,
+  followers: 123,
+  following: 123
+}
