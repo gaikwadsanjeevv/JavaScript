4623bdb0371ca38d6d0fc63e462fd0807dc3d14e
Sanjeev V Gaikwad <161517224+gaikwadsanjeevv@users.noreply.github.com>
2024-11-04 10:46:09 -0800

    Create objectDemo.js
    Object demo

---
+++ objectDemo.js
@@ -0,0 +1,20 @@
+const student = {
+    fullName: "Sanjeev Gaikwad",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true,
+};
+
+let Shopper = {
+    productName: "Electronics",
+    subProduct: "Iphone",
+    iphoneType: "iphone 16",
+    cost: "12000",
+    Available: 5,
+};
+
+console.log(student);
+console.log(student["fullName"]);
+
+console.log(Shopper);
+console.log(Shopper["cost"]); 
